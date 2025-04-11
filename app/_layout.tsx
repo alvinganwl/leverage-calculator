2eebf66ba06f515b66689542e5e0d2036ada6e98
alvinganwl <98844942+alvinganwl@users.noreply.github.com>
2025-04-11 17:26:11 +0800

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactNode } from "react";
+import "./globals.css";  // 如果有样式文件的话，别忘了导入
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -9,4 +10,3 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     </html>
   );
 }
-
